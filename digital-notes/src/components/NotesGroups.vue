dded05d4ef667518f81f0e2d9ec1ae97f21334ee
stonefarmer9 <g.p.levaillant@gmail.com>
2021-08-18 15:05:46 +0100

    FEAT: notesGroup renders a checkbox list of selectable noteGroups

---
+++ digital-notes/src/components/NotesGroups.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+        <li v-for="noteGroup in noteGroups" :key="noteGroup.id">
+            <input type="checkbox" :id="noteGroup.id" :value="noteGroup.name" v-model="selectedNoteGroups" v-on:change="updateSelectedGroups"/>
+            {{ noteGroup.name }}
+        </li>
+
+    </ul>
+
+</template>
+
+<script>
+export default {
+  name: 'NotesGroups',
+
+  data () {
+    return {
+      selectedNoteGroups: []
+    }
+  },
+  props: {
+    noteGroups: Array
+  },
+  methods: {
+    'updateSelectedGroups' () {
+      this.$emit('updateSelectedNoteGroups', this.selectedNoteGroups)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- digital-notes/tests/unit/NotesGroup.spec.js
+++ digital-notes/tests/unit/NotesGroup.spec.js
@@ -1,28 +1,39 @@
-import { mount } from '@vue/test-utils';
-import NotesGroups from '@/components/NotesGroups.vue';
+import { mount } from '@vue/test-utils'
+import NotesGroups from '@/components/NotesGroups.vue'
+import { MOCK_NOTE_GROUPS } from './testHelpers/testHelperConstants.js'
 
-describe("NotesGroups.vue", () => {
-    let wrapper;
-    beforeAll(() => {
-        wrapper = mount(NotesGroups)
+describe('NotesGroups.vue', () => {
+  let wrapper
+  beforeAll(() => {
+      wrapper = mount(NotesGroups, {
+        props: { noteGroups: MOCK_NOTE_GROUPS }
     })
+  })
+  afterAll(() => {
+    wrapper.destroy()
+  })
     
-    it("Renders a list of note groups", () => {
-        expect(wrapper.findAll('li')).toHaveLength(4);
-    })
-    it("Renders a checkbox in each list item", () => {
-        expect(wrapper.findAll('li > input')).toHaveLength(4);
-    })
+  it('Renders a list of note groups', () => {
+    expect(wrapper.findAll('li')).toHaveLength(4)
+  })
+  it('Renders a checkbox in each list item', () => {
+    expect(wrapper.findAll('li > input')).toHaveLength(4)
+  })
 })
 
-describe("NotesGroup checkbox functionality", () => {
-    let wrapper;
-    beforeAll(() => {
-        wrapper = mount(NotesGroups);
+describe('NotesGroup checkbox functionality', () => {
+  let wrapper
+  beforeAll(() => {
+      wrapper = mount(NotesGroups, {
+        props: { noteGroups: MOCK_NOTE_GROUPS }
     })
-    it("Selecting a checkbox emits the selected function", async () => {
-        await wrapper.find('input[name=quests]').setValue()
+  })
+  afterAll(() => {
+    wrapper.destroy()
+  })
+  it('Selecting a checkbox emits the selected function', async () => {
+    await wrapper.find('input[type=checkbox]').setValue()
 
-        expect(wrapper.emitted()).toHaveProperty('groupSelected')
-    })
+    expect(wrapper.emitted()).toHaveProperty('updateSelectedNoteGroups')
+  })
 })
