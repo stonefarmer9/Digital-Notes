55b095709818fba17b80944f5937c8ffeb10a320
gambit <gambit@xavierschool.com>
2021-08-18 12:23:27 +0100

    FEAT: Table renders props data and shows watermark when no data passed. Tests passing

--- digital-notes/src/components/NotesTable.vue
+++ digital-notes/src/components/NotesTable.vue
@@ -1,12 +1,30 @@
 <template>
+<div>
     <table>
-        
+        <tr>
+            <th>Notes Group</th>
+            <th>Title</th>
+            <th>Date Created</th>
+        </tr>
+        <tbody v-if="notes.length > 0">
+            <tr v-for="note in notes" :key="note.id" class="table-row">
+                <td>{{ note.noteGroup }}</td>
+                <td>{{ note.title }}</td>
+                <td>{{ note.dateCreated }}</td>
+            </tr>
+        </tbody>
+        <span v-if="notes.length === 0">No data for table</span>
+
     </table>
+</div>
 </template>
 
 <script>
 export default {
-    name: 'NotesTable'    
+  name: 'NotesTable',
+  props: {
+    notes: Array
+  }
 }
 </script>
 
--- digital-notes/src/views/Home.vue
+++ digital-notes/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <H1>LETS NOTE</H1>
+    <NoteTable
+      :notes="notesData"
+    />
   </div>
 </template>
 
@@ -10,7 +13,36 @@ import NoteTable from '@/components/NotesTable.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    NoteTable
+  },
+  data () {
+    return {
+      notesEmpty: [],
+      notesData: [
+        {
+          id: 1,
+          title: 'Note Title One',
+          noteGroup: 'quests',
+          dateCreated: '18/08/2021',
+          note: 'This is a note about one thing or another'
+        },
+        {
+          id: 2,
+          title: 'Note Title Two',
+          noteGroup: 'npcs',
+          dateCreated: '18/08/2021',
+          note: 'This is a note about two things or another'
+        },
+        {
+          id: 3,
+          title: 'Note Title Three',
+          noteGroup: 'locations',
+          dateCreated: '18/08/2021',
+          note: 'This is a note about three things or another'
+        }
+
+      ]
+    }
   }
 }
 </script>
