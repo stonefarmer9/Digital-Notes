02296fe6b2d36d48c85521c7cf1ff8e7c5f57d42
gambit <gambit@xavierschool.com>
2021-08-18 12:08:19 +0100

    TEST: updated test to test table length and table render

--- digital-notes/tests/unit/NotesTable.spec.js
+++ digital-notes/tests/unit/NotesTable.spec.js
@@ -1,13 +1,29 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { MOCK_TABLE_DATA } from './testHelperContants'
 import NotesTable from '@/components/NotesTable.vue'
 
 describe('NotesTable.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(NotesTable, {
-      props: { MOCK_TABLE_DATA }
-    })
-    expect(wrapper.findAll('tr').length).toBe(3)
+  let wrapper;
+  let table;
+  let rows;
+  beforeAll(() => {
+    wrapper = mount(NotesTable, {
+      props: { notes: MOCK_TABLE_DATA }
+    });
+    table = wrapper.find("table");
+    rows = wrapper.findAll("tbody > tr")
+    
+  })
+
+  afterAll(() => {
+    wrapper.destroy()
+  })
+  it('renders a table', () => {
+    expect(table.exists()).toBe(true)
+
+  
+  })
+  it("renders the notes passed as table rows", () => {
+    expect(rows).toHaveLength(3)
   })
 })
