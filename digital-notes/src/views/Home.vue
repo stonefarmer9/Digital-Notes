f483faa4b85e8c6892f045cb6c7e2906b4296d76
stonefarmer9 <g.p.levaillant@gmail.com>
2021-08-19 08:09:28 +0100

    FEAT: NoteDisplay renders a note in a DL - test passing

--- digital-notes/src/views/Home.vue
+++ digital-notes/src/views/Home.vue
@@ -9,31 +9,42 @@
     <NoteTable
       :notes="notesData"
     />
+    <NoteDisplay
+      :selectedNote="selectedNote"
+      @selectNote="selectNote"
+      />
   </div>
 </template>
 
 <script>
 import NoteTable from '@/components/NotesTable.vue'
 import NotesGroups from '@/components/NotesGroups.vue'
-import { MOCK_NOTE_GROUPS, MOCK_TABLE_DATA } from '../../tests/unit/testHelpers/testHelperConstants.js'
+import NoteDisplay from '@/components/NoteDisplay.vue'
+import { MOCK_NOTE_GROUPS, MOCK_TABLE_DATA, MOCK_NOTE } from '../../tests/unit/testHelpers/testHelperConstants.js'
 
 export default {
   name: 'Home',
   components: {
     NoteTable,
-    NotesGroups
+    NotesGroups,
+    NoteDisplay
   },
   data () {
     return {
       notesEmpty: [],
       notesData: MOCK_TABLE_DATA,
       noteGroups: MOCK_NOTE_GROUPS,
-      selectedNoteGroups: []
+      selectedNoteGroups: [],
+      selectedNote: MOCK_NOTE
     }
   },
   methods: {
     'updateSelectedNoteGroups' (selectedGroups) {
       this.selectedNoteGroups = selectedGroups
+    },
+    'selectNote' (note) {
+      console.log('selecting')
+      this.selectedNote = note
     }
   }
 }
