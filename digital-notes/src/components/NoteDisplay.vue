08fc92124c7533d8ddd18503d24fea677b088af3
stonefarmer9 <g.p.levaillant@gmail.com>
2021-08-19 08:09:28 +0100

    FEAT: NoteDisplay renders a note in a DL - test passing

---
+++ digital-notes/src/components/NoteDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+    <dl v-for="(value, key) in noteObject" :key="key">
+        <dt v-if="key != 'id'"> {{ key }}</dt>
+        <dd v-if="key != 'id'">{{ value }}</dd>
+    </dl>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NoteDisplay',
+
+  props: {
+    selectedNote: Object
+  },
+  computed: {
+    noteObject () {
+      const note = { ...this.selectedNote }
+      delete note.id
+
+      return note
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
