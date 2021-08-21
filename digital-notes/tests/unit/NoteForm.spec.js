24413899278f6fa0a2e041e6539110ced3db5bfd
stonefarmer9 <g.p.levaillant@gmail.com>
2021-08-21 11:28:17 +0100

    TEST: Form renders labels in correct order

--- digital-notes/tests/unit/NoteForm.spec.js
+++ digital-notes/tests/unit/NoteForm.spec.js
@@ -27,3 +27,29 @@ describe('NoteForm.vue renders the correct input fields', () => {
       expect(wrapper.findComponent(TagInput).exists()).toBe(true)
   })
 })
+
+describe('NoteFrom.vue renders labels for each input', () => {
+     let wrapper
+
+    beforeEach(() => {
+        wrapper = mount(NoteForm)
+    })
+    afterEach(() => {
+        wrapper = null
+    })
+    afterAll(() => {
+        wrapper.destroy()
+    })
+    it('It renders a label for Title,  Note Details Note Group, Note Tags, in that order', () => {
+        const Labels = wrapper.findAll('label')
+        const TitleLabel = Labels[0]
+        const NoteDetailsLabel = Labels[1]
+        const NoteGroupsLabel = Labels[2]
+        const NoteTagsLabel = Labels[3]
+
+        expect(TitleLabel.text()).toContain('Title')
+        expect(NoteDetailsLabel.text()).toContain('Note')
+        expect(NoteGroupsLabel.text()).toContain('Groups/Categories')
+        expect(NoteTagsLabel.text()).toContain('Tags')
+    })
+})
